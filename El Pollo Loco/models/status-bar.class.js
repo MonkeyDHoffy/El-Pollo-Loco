a18941f761169071944c0c8aa193ab0099450833
MonkeyDHoffy <jonekstoff@googlemail.com>
2025-07-01 19:31:33 +0200

    fix

--- El Pollo Loco/models/status-bar.class.js
+++ El Pollo Loco/models/status-bar.class.js
@@ -9,6 +9,24 @@ class StatusBar extends DrawableObject {
     'img/img_pollo_locco/img/7_statusbars/1_statusbar/2_statusbar_health/orange/100.png'
   ];
 
+  IMAGES_COINS = [
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/coin/0.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/coin/20.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/coin/40.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/coin/60.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/coin/80.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/1_statusbar_coin/coin/100.png'
+  ];
+
+  IMAGES_BOTTLE = [
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/bottle/0.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/bottle/20.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/bottle/40.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/bottle/60.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/bottle/80.png',
+    'img/img_pollo_locco/img/7_statusbars/1_statusbar/3_statusbar_bottle/bottle/100.png'
+  ];
+
   percentage = 100; // Lebenspunkte in Prozent
 
   constructor() {
@@ -21,9 +39,17 @@ class StatusBar extends DrawableObject {
     this.setPercentage(100); // Setzt die Lebenspunkte auf 100%
   }
 
-  setPercentage(percentage) {
+  setPercentage(percentage,value) {
     this.percentage = percentage;
-    let path = this.IMAGES_LIFEPOINTS[this.resolveImageIndex()];
+    let images;
+    if (value === 'coins') {
+      images = this.IMAGES_COINS;
+    } else if (value === 'bottle') {
+      images = this.IMAGES_BOTTLE;
+    } else {
+      images = this.IMAGES_LIFEPOINTS;
+    }
+    let path = images[this.resolveImageIndex()];
     this.img = this.imageCache[path];
   }
 
